47e14a188de3b4a7fcdee1979a4d347c868c2f89
Mohammad Khan <mkhan@scienceskillshs.com>
2017-03-10 14:29:30 +0000

    tinyturtles

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 TinyTurtle.apply(window);
 
 // Type your directions to Tiny Turtle below
-
-
+forward(40);
+right(90);
+forward(30);
+right(90);
+forward(40);
+right(90);
+forward(30);
+right(90);
+forward(40);
+right(30);
+forward(30);
+right(120);
+forward(30);
 
 stamp();
